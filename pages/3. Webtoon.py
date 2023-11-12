82eddf8f258a7baf0dc5adb791662bc51c9fb649
RkFeeder <149927934+RkFeeder@users.noreply.github.com>
2023-11-12 09:11:28 +0900

    Update 3. Webtoon.py

--- pages/3. Webtoon.py
+++ pages/3. Webtoon.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open(r'C:\Users\randy_dywt\Downloads\Python\hackathon\pages\Webtoon.png')
+image = Image.open(r'\hackathon\pages\Webtoon.png')
 
 st.image(image, caption = '졸업 요건 웹툰')
